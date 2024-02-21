c8a8d2c50309d179828a72540197182f5fa85b66
binson thangjam <binsonsingh@gmail.com>
2024-02-21 10:23:20 +0530

    fixed errors pages

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { ifError } = require("assert");
 
 const server = http.createServer((req, res) => {
-  let pathName = "./" + req.url;
+  let pathName = "." + req.url;
+  if (pathName === "./") {
+    pathName += "index.html";
+  }
+  console.log(pathName);
 
   fs.readFile(pathName, {}, (err, content) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    if (err) {
+      fs.readFile("./404.html", {}, (err, content) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("page not found");
+        } else {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(content);
+        }
+      });
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
 
-    res.end(content);
+      res.end(content);
+    }
   });
 });
 
